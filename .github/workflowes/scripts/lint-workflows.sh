3af143d9109303c36531b8435903477bf68028d0
Keys <70819367+keys-i@users.noreply.github.com>
2025-10-17 09:26:31 +1000

    Upgrade the pinned workflow linter to 1.7.8

--- .github/workflowes/scripts/lint-workflows.sh
+++ .github/workflowes/scripts/lint-workflows.sh
@@ -7,5 +7,5 @@ docker run --rm --network none \
     --mount "type=bind,src=$PWD/.github/workflows,dst=/repo/.github/workflows,readonly" \
     --mount "type=bind,src=$PWD/examples,dst=/repo/examples,readonly" \
     --workdir /repo \
-    docker.io/rhysd/actionlint:1.7.7@sha256:887a259a5a534f3c4f36cb02dca341673c6089431057242cdc931e9f133147e9 \
+    docker.io/rhysd/actionlint:1.7.8@sha256:96d4a8c87dbbfb3bdd324f8fdc285fc3df5261e2decc619a4dd7e8ee52bbfd46 \
     -color .github/workflows/*.yml examples/github-actions.yml
